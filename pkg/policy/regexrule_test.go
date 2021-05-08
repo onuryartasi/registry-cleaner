d5b8a3eab817913cd845498e0eb686cd0663cd55
Onur Yartaşı <onuryartasi@live.com>
2021-05-08 14:49:11 +0300

    fix lint problem

--- pkg/policy/regexrule_test.go
+++ pkg/policy/regexrule_test.go
@@ -1,23 +1,20 @@
 package policy
 
 import (
-	"github.com/onuryartasi/registry-cleaner/pkg/registry"
 	"testing"
+
+	"github.com/onuryartasi/registry-cleaner/pkg/registry"
 )
 
-func TestRegexRuleCheck(t *testing.T){
+func TestRegexRuleCheck(t *testing.T) {
 	policy := Policy{}
 	policy.RegexRule.Pattern = []string{"foo/bar"}
 
 	image := registry.Image{Name: "foo/bars"}
-	type Image struct {
-		Name string   `json:"name"`
-		Tags []string `json:"tags"`
-	}
 	actually := policy.regexRuleCheck(image)
 
 	if actually.Name != image.Name {
-		t.Errorf("regexRuleCheck failed. expected %v, got %v",image.Name,actually.Name)
+		t.Errorf("regexRuleCheck failed. expected %v, got %v", image.Name, actually.Name)
 	}
 
 }
